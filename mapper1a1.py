5053d3299e64bbf5a1bc651a329c351798dff2de
Do Son Tung <tungds2001@gmail.com>
2024-04-06 21:09:21 -0400

    minor change

--- mapper1a1.py
+++ mapper1a1.py
@@ -12,6 +12,8 @@ for line in sys.stdin:
     
     fields = line.strip().split(',')
     violation_time = fields[19]
-
+    if violation_time == '':
+        continue
     hour = violation_time[0:2] + violation_time[4] # get hour, remove minute
+    
     print('%s\t1' % (hour))  # Use tab as delimiter
