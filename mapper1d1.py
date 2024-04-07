3927081131a834b542ce2a71757a5adc63a1d2e8
Do Son Tung <tungds2001@gmail.com>
2024-04-06 22:17:34 -0400

    minor change

--- mapper1d1.py
+++ mapper1d1.py
@@ -15,5 +15,16 @@ for line in sys.stdin:
     # Filter out all missing values
     if vehicle_color == '':
         continue
+    # Accumulate different names into same categories
+    if vehicle_color in ['GR', 'GREY', 'GRY']:
+        vehicle_color = 'GRAY'
+    elif vehicle_color in ['YELLO', 'YELLOW', 'GLD']:
+        vehicle_color = 'GOLD'
+    elif vehicle_color in ['RD', 'R']:
+        vehicle_color = 'RED'
+    elif vehicle_color in ['BL', 'BK', 'BLK']:
+        vehicle_color = 'BLACK'
+    elif vehicle_color in ['WH']:
+        vehicle_color = 'WHITE'
 
     print('%s\t1' % (vehicle_color))  # Use tab as delimiter
