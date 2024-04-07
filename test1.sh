725295c187dc6af9ce9fcccd0dd39d48fb663b61
Do Son Tung <tungds2001@gmail.com>
2024-04-06 21:40:58 -0400

    minor change

--- test1.sh
+++ test1.sh
@@ -15,12 +15,60 @@ input_file=$1
 # MapReduce Round 1
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file /mapreduce-test/cisc5950-project1/mapper1a1.py -mapper /mapreduce-test/cisc5950-project1/mapper1a1.py \
--file /mapreduce-test/cisc5950-project1/reducer1a1.py -reducer /mapreduce-test/cisc5950-project1/reducer1a1.py \
+-file /mapreduce-test/cisc5950-project1/reducer11.py -reducer /mapreduce-test/cisc5950-project1/reducer11.py \
 -input /project1/input/* -output /project1/tmp/
 # MapReduce Round 2
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file /mapreduce-test/cisc5950-project1/mapper1a2.py -mapper /mapreduce-test/cisc5950-project1/mapper1a2.py \
--file /mapreduce-test/cisc5950-project1/reducer1a2.py -reducer /mapreduce-test/cisc5950-project1/reducer1a2.py \
+-file /mapreduce-test/cisc5950-project1/mapper12.py -mapper /mapreduce-test/cisc5950-project1/mapper12.py \
+-file /mapreduce-test/cisc5950-project1/reducer12.py -reducer /mapreduce-test/cisc5950-project1/reducer12.py \
+-input /project1/tmp/* -output /project1/output/
+# Output and clean up output
+/usr/local/hadoop/bin/hdfs dfs -cat /project1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /project1/tmp/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /project1/output/
+
+# Question 1b
+# MapReduce Round 1
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
+-file /mapreduce-test/cisc5950-project1/mapper1b1.py -mapper /mapreduce-test/cisc5950-project1/mapper1b1.py \
+-file /mapreduce-test/cisc5950-project1/reducer11.py -reducer /mapreduce-test/cisc5950-project1/reducer11.py \
+-input /project1/input/* -output /project1/tmp/
+# MapReduce Round 2
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
+-file /mapreduce-test/cisc5950-project1/mapper12.py -mapper /mapreduce-test/cisc5950-project1/mapper12.py \
+-file /mapreduce-test/cisc5950-project1/reducer12.py -reducer /mapreduce-test/cisc5950-project1/reducer12.py \
+-input /project1/tmp/* -output /project1/output/
+# Output and clean up output
+/usr/local/hadoop/bin/hdfs dfs -cat /project1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /project1/tmp/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /project1/output/
+
+# Question 1c
+# MapReduce Round 1
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
+-file /mapreduce-test/cisc5950-project1/mapper1c1.py -mapper /mapreduce-test/cisc5950-project1/mapper1c1.py \
+-file /mapreduce-test/cisc5950-project1/reducer11.py -reducer /mapreduce-test/cisc5950-project1/reducer11.py \
+-input /project1/input/* -output /project1/tmp/
+# MapReduce Round 2
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
+-file /mapreduce-test/cisc5950-project1/mapper12.py -mapper /mapreduce-test/cisc5950-project1/mapper12.py \
+-file /mapreduce-test/cisc5950-project1/reducer12.py -reducer /mapreduce-test/cisc5950-project1/reducer12.py \
+-input /project1/tmp/* -output /project1/output/
+# Output and clean up output
+/usr/local/hadoop/bin/hdfs dfs -cat /project1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /project1/tmp/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /project1/output/
+
+# Question 1d
+# MapReduce Round 1
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
+-file /mapreduce-test/cisc5950-project1/mapper1d1.py -mapper /mapreduce-test/cisc5950-project1/mapper1d1.py \
+-file /mapreduce-test/cisc5950-project1/reducer11.py -reducer /mapreduce-test/cisc5950-project1/reducer11.py \
+-input /project1/input/* -output /project1/tmp/
+# MapReduce Round 2
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
+-file /mapreduce-test/cisc5950-project1/mapper12.py -mapper /mapreduce-test/cisc5950-project1/mapper12.py \
+-file /mapreduce-test/cisc5950-project1/reducer12.py -reducer /mapreduce-test/cisc5950-project1/reducer12.py \
 -input /project1/tmp/* -output /project1/output/
 # Output and clean up output
 /usr/local/hadoop/bin/hdfs dfs -cat /project1/output/part-00000
